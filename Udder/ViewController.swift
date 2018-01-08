5dacecadb74ef199b392abd7c9fcc20c2c323213
Matias Cardozo <matiascardozo@mac-mini-de-matias.local>
2018-01-08 18:40:15 -0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Matias Cardozo on 8/1/18.
+//  Copyright © 2018 MICL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
